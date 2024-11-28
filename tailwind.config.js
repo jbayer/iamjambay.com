75a5923fa4032089b1436699a40815c709a798d9
Apurva Mehta <830220+apurvam@users.noreply.github.com>
2024-11-28 11:29:14 -0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+      content: ["./src/**/*.{html,md,njk,ejs,pug}"],
+      theme: {
+              extend: {},
+            },
+      plugins: [],
+};
+
